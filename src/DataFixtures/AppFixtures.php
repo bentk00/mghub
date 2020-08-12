0fcd21ecaff40dc598028b313a007bc6e255c92f
Younes Benachour <benachour.younes92@gmail.com>
2020-08-12 02:49:42 +0100

    Mise en place de l'application et de la bdd

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+
+        $manager->flush();
+    }
+}
