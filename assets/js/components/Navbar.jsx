f708cfc832177bab9c8286fc8cc8ccabe8e83b1d
Younes Benachour <benachour.younes92@gmail.com>
2020-08-15 00:55:40 +0100

    navbar fix

--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -16,19 +16,19 @@ function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarColor01">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" style={{fontSize: '16px'}} href="#">STORE !</a>
+                            <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" style={{fontSize: '16px'}} href="#">STORE !</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">News</a>
+                            <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">News</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Tests</a>
+                            <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">Tests</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link"  href="#">
+                            <a className="nav-link"  data-toggle="collapse" data-target=".navbar-collapse.show" href="#">
                                 Events</a>
                         </li>
                         <li className="nav-item dropdown">
--- assets/js/components/SecondNavbar.jsx
+++ assets/js/components/SecondNavbar.jsx
@@ -6,41 +6,45 @@ function SecondNavbar(props) {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#secNavBar"
+                        aria-controls="secNavBar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
                 <div className="d-md-flex d-block flex-row mx-md-auto mx-0">
-                    <div className="collapse navbar-collapse" id="navbarColor01">
-                        <ul className="navbar-nav">
+                    <div className="collapse navbar-collapse" id="secNavBar">
+                        <ul className="navbar-nav mr-auto">
                             <li className="nav-item mr-5">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">
                                     <i className="mr-1"><FontAwesomeIcon icon={['fab', 'playstation']}/></i>
                                     PS5
                                 </a>
                             </li>
                             <li className="nav-item mr-5">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">
                                     <i className="mr-1"><FontAwesomeIcon icon={['fab', 'xbox']} /></i>
                                     SERIES X
                                 </a>
                             </li>
                             <li className="nav-item mr-5">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">
                                     <i className="mr-1"><FontAwesomeIcon icon={['fab', 'playstation']}/></i>
                                     PS4
                                 </a>
                             </li>
                             <li className="nav-item mr-5">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">
                                     <i className="mr-1"><FontAwesomeIcon icon={['fab', 'xbox']} /></i>
                                     ONE / ONE X
                                 </a>
                             </li>
                             <li className="nav-item mr-5">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">
                                     <i className="mr-1"><FontAwesomeIcon icon={['fab', 'windows']} /></i>
                                     PC Master race
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" href="#">
                                     <i className="mr-1"><FontAwesomeIcon icon={['fab', 'steam']} /></i>
                                     Steam
                                 </a>
